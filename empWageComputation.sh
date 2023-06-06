cbdb967fa472a16a76a5b1df8ddb050ddd8b7f75
Neelesh2909 <rawat.neelesh9@gmail.com>
2023-06-06 08:59:14 +0530

    [Neelesh] Refactor: calculate employee's wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+wagePerHour=20
+workingHours=0
 
 randomCheck=$((RANDOM%2))
 if (( $randomCheck == 1 ))
 then
 	echo "Employee is Present"
+	workingHours=8
 else
 	echo "Employee is Absent"
 fi
+salary=$(($workingHours * $wagePerHour))
+echo "Salary of employee is : " $salary
+
