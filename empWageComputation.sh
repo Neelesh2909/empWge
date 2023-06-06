0d532a4c99655df5791ad3140dc983dc539224ec
Neelesh2909 <rawat.neelesh9@gmail.com>
2023-06-06 08:41:27 +0530

    [Neelesh] Add: checking for an employee's presence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+
+randomCheck=$((RANDOM%2))
+if (( $randomCheck == 1 ))
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
